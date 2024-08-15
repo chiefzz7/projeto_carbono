532229f7d391f04ec6e4a3acaafb177dfd5ad876
AMS-DS1 <fatec-ams-ds1@academico.intra>
2024-08-15 12:15:38 -0300

    alteração final

--- script.js
+++ script.js
@@ -1,23 +1,34 @@
-window.alert("teste de link html/js, antes de apresentar apagar a linha 1 completa");
-
 function calcular() {
     var nome = document.getElementById('nome').value;
     var kms = parseFloat(document.getElementById('kms').value);
+    var numAssento = parseFloat(document.getElementById('numAssento').value)
 
     //Calculo
 
-    var result = kms*2*200
+    var result = (kms*2*200) 
+    var qtdCo2 = result / numAssento
+    var qtdArvore = qtdCo2 / 156
+
+    result = result.toFixed(3);
+    qtdCo2 = qtdCo2.toFixed(3);
+    qtdArvore = qtdArvore.toFixed(3);
 
     //Armazenar os dados no localStorage
     localStorage.setItem('nome', nome);
     localStorage.setItem('resultado', result);
+    localStorage.setItem('numAssento', numAssento);
+    localStorage.setItem('qtdCo2', qtdCo2);
+    localStorage.setItem('qtdArvore', qtdArvore);
 }
 
 window.onload = function() {
     var nome = localStorage.getItem('nome');
     var result = localStorage.getItem('resultado');
+    var numAssento = localStorage.getItem('numAssento');
+    var qtdCo2 = localStorage.getItem('qtdCo2');
+    var qtdArvore = localStorage.getItem('qtdArvore')
 
     if (nome && result) {
-        document.getElementById('exibirResultado').innerText = `${nome}, os quilometros gastos por ano é  ${result}Km.`;
+        document.getElementById('exibirResultado').innerText = `Olá ${nome}, a quantidade consumida de CO₂ é de ${qtdCo2}/Por Ano, e o número de árvores que você deveria plantar para compensar será de ${qtdArvore}/Por Ano`;
     }
 }
